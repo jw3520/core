b2fe5ffdd8f5aa93df4e7d9c230b97838346254c
jw3520 <jw3520@daum.net>
2022-05-24 22:23:07 +0900

    의존 주입 방법 - 생성자 주입 - 수정자 주입 - 필드 주입 - 일반 메서드 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,9 +8,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
-    private final MemberRepository memberRepository;
+
+    // 필드 주입
+//    @Autowired private memberRepository memberRepository;
+//    @Autowired
+//    private DiscountPolicy discountPolicy;
+
+    private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
+    //수정자 주입
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    //생성자 주입
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
@@ -23,6 +41,12 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice);
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+    // 일반 메서드 주입
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     //TEST
     public MemberRepository getMemberRepository() {
